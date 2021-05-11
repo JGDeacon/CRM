aa9a2b6cc0fc91d3ebd6b5cf2e29059f639af68c
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-05-11 15:51:11 -0400

    Check in before the mass find replace Guid to Int

---
+++ CRMServices/AdministrationService.cs
@@ -0,0 +1,34 @@
+﻿using CRMData;
+using CRMModels.Create;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMServices
+{
+    public class AdministrationService
+    {
+        protected readonly ApplicationDbContext ctx = new ApplicationDbContext();
+        private readonly Guid _userID;
+        static HistoryService historyService = new HistoryService();
+
+        public AdministrationService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+        public bool AddDepartment(CreateDepartment model)
+        {
+            ctx.Departments.Add(new Departments
+            {
+                CompanyID = model.CompanyID,
+                DepartmentName = model.DepartmentName,
+                CreatedDateUTC = DateTimeOffset.UtcNow
+            });
+            return ctx.SaveChanges() == 1;
+        }
+        public bool DeleteDepartment(int)
+    }
+}
