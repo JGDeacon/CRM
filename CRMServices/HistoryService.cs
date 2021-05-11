2abed1eac1ebc6b66333a20d33eaab0f67b141a9
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-05-11 15:37:05 -0400

    History Service First Revision

---
+++ CRMServices/HistoryService.cs
@@ -0,0 +1,55 @@
+﻿using CRMData;
+using CRMModels.Create;
+using CRMModels.Read;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMServices
+{
+    public class HistoryService
+    {
+        public HistoryService()
+        {
+
+        }
+
+        public bool AddHistory(CreateHistory model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.History.Add(new History
+                {
+                    CompanyID = model.CompanyID,
+                    UserID = model.UserID,
+                    Table = model.Table,
+                    GuidID = model.GuidID,
+                    IntID = model.IntID,
+                    Change = model.Change,
+                    CreatedDateUTC = DateTimeOffset.UtcNow
+                });
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<ReadHistory> GetHistory(Guid CompanyID)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.History.Where(e => e.CompanyID == CompanyID).Select(f => new ReadHistory
+                {
+                    HistoryID = f.HistoryID,
+                    CompanyName = ctx.Companies.FirstOrDefault(g => g.CompanyID == f.CompanyID).CompanyName,
+                    Username = ctx.Users.FirstOrDefault(h => h.Id.ToString() == f.UserID.ToString()).UserName,
+                    Table = f.Table,
+                    GuidID = f.GuidID,
+                    IntID = f.IntID,
+                    Change = f.Change,
+                    CreatedDateUTC = f.CreatedDateUTC
+                });
+                return query.ToList();
+            }
+        }
+    }
+}
